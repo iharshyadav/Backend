1428d6006830437bea53d0de5d7afba3cc7c485a
Harsh Yadav <hy945196@gmail.com>
2024-01-24 06:58:26 +0530

    user & video model , jwt ,bcrypt,mongoose aggregation-pipeline

--- src/app.js
+++ src/app.js
@@ -9,6 +9,11 @@ app.use(cors({
     credentials:true,
 }))
 
+app.get("/",(req,res)=>{
+  res.send("hello")
+//   console.log("hello")
+})
+
 app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extended: true, limit:"16kb"}));
 app.use(express.static("public"));
